7493612218f04896592763093047869c08f123d5
simpson <450054@naver.com>
2026-01-12 15:24:09 +0900

    [2025.01.12 simpson]

--- app/routes.ts
+++ app/routes.ts
@@ -2,6 +2,9 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
+  route("login", "routes/login.tsx"),
+  route("signup", "routes/signup.tsx"),
+  route("logout", "routes/logout.tsx"),
   route("characters", "routes/characters.tsx"),
   route("weekly", "routes/weekly.tsx"),
   route("daily", "routes/daily.tsx"),
